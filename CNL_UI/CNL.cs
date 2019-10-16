a497d8794bdba0fc172ba3f4bedb246fa0491034
ErsinCANLI <ersin.canli@gmail.com>
2019-10-16 13:53:55 +0300

    Ersin CANLI

--- CNL_UI/CNL.cs
+++ CNL_UI/CNL.cs
@@ -10,9 +10,9 @@ using System.Windows.Forms;
 
 namespace CNL_UI
 {
-    public partial class Form1 : Form
+    public partial class CNL : Form
     {
-        public Form1()
+        public CNL()
         {
             InitializeComponent();
         }
